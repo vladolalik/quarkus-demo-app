5efd0b30cc88963c778c7cf6583798abae93e929
Vladimir Lalik <Vladimir.Lalik1@sk.ibm.com>
2021-05-25 15:44:11 +0200

    update hello endpoint

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -16,7 +16,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "hello from quarkus";
     }
 
     @Path("{name}")
